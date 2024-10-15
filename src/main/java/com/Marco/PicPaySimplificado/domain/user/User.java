213d0a06e2d0f85ccd5c757c1f9534160b776ac3
MarcoShvy <mass7813@gmail.com>
2024-10-15 08:53:16 -0300

    User created

---
+++ src/main/java/com/Marco/PicPaySimplificado/domain/user/User.java
@@ -0,0 +1,39 @@
+package com.Marco.PicPaySimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/com/Marco/PicPaySimplificado/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.Marco.PicPaySimplificado.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
