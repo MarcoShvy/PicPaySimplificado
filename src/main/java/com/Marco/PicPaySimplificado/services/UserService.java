95799049fae3ee2a99c4ed9fad7a71cbe9de2e35
MarcoShvy <mass7813@gmail.com>
2024-10-18 10:19:56 -0300

    User Service

---
+++ src/main/java/com/Marco/PicPaySimplificado/services/UserService.java
@@ -0,0 +1,35 @@
+package com.Marco.PicPaySimplificado.services;
+
+import com.Marco.PicPaySimplificado.domain.user.User;
+import com.Marco.PicPaySimplificado.domain.user.UserType;
+import com.Marco.PicPaySimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+@Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuario lojista não pode realizar transferência");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public Optional<User> findUserById(Long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+}
