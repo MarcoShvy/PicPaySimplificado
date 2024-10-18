fd972ccb7eb0838435c5b38a613340d95ee2990b
MarcoShvy <mass7813@gmail.com>
2024-10-18 10:11:47 -0300

    repository findByDocument

--- src/main/java/com/Marco/PicPaySimplificado/repositories/UserRepository.java
+++ src/main/java/com/Marco/PicPaySimplificado/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.Marco.PicPaySimplificado.repositories;
 import com.Marco.PicPaySimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
 }
